d98f094ad67fbf17fdd28af9680d40b10fd4c231
Dan Peters <daniel.r.peters@icloud.com>
2016-07-28 14:27:47 -0700

    refactored to fit python formatting and made it into a class, versus individual defs

--- membershipData.py
+++ membershipData.py
@@ -1,69 +1,64 @@
 import pandas as pd
 
-# this only needed for determining version numbers
-import sys
-
 # improve portability
 import os
 
-# function to load and prep csv data
-def loadData():
 
-    # these should be made more flexible so I can load any file
-    folder = r"C:\Users\daniel\scripts\membershipcalculator"
-    inFile = r"testdata.csv"
-    # outFile = r'membershipNumbers.csv'  #this is probably not needed right here?
+class MembershipCalc (object):
+    def __init__(self):
+        self.folder = r'C:\Users\daniel\scripts\membershipcalculator'
+        self.in_file = r'testdata.csv'
+        self.out_file = r'membershipNumbers.csv'
+        self.in_path = os.path.join(self.folder, self.in_file)
+        self.outPath = os.path.join(self.folder, self.out_file)
+        self.df = pd.read_csv(self.in_path)
+        self.t_date = 'Transaction Date'
+        self.e_date = 'Purchased Membership Expiration Date'
+        self.mem_id_col = 'Membership ID'
 
-    inPath = os.path.join(folder, inFile)
+    # function to load and prep csv data
+    def load_data(self):
 
-    # outPath = os.path.join(folder, outFile) # not used here
+        # make sure that dates are in right format and sort by transaction date
+        self.df[self.t_date] = pd.to_datetime(self.df[self.t_date], infer_datetime_format=True)
+        self.df[self.e_date] = pd.to_datetime(self.df[self.e_date], infer_datetime_format=True)
+        self.df = self.df.sort_values([self.t_date, self.mem_id_col], ascending=[1, 1])
 
-    df = pd.read_csv(inPath)
-    
-    # these should be made more flexible so I can change what columns are used
-    tDateCol = 'Transaction Date'
-    eDateCol = 'Purchased Membership Expiration Date'
-    memIDCol = 'Membership ID'
+        # DEBUG
+        # print (df.head(10))
 
-    # make sure that dates are in right format and sort by transaction date
-    df[tDateCol] = pd.to_datetime(df[tDateCol], infer_datetime_format=True)
-    df[eDateCol] = pd.to_datetime(df[eDateCol], infer_datetime_format=True)
-    df = df.sort_values([tDateCol, memIDCol], ascending=[1, 1])
-    
-    # DEBUG
-    # print (df.head(10))
-    
-    return df
+        return 0
 
-# function to return a new csv with the number of memberships added and dropped by date
-def calculateMembership(tdata):
+    # function to return a new csv with the number of memberships added and dropped by date
+    def calculate_membership(self):
 
-    beginDate = tdata['Transaction Date'].min()
+        begin_date = self.df[self.t_date].min()
 
-    endDate = tdata['Purchased Membership Expiration Date'].max()
+        end_date = self.df[self.e_date].max()
 
-    # DEBUG
+        # DEBUG
 
-    print('beginDate = ', beginDate)
+        print('beginDate = ', begin_date)
 
-    print('endDate = ', endDate)
+        print('endDate = ', end_date)
 
-    # create the new dataframe indexed with complete range of dates
-    dRange = pd.date_range(beginDate, endDate, freq='D')
+        # create the new dataframe indexed with complete range of dates
+        dRange = pd.date_range(begin_date, end_date, freq='D')
 
-    # for each row in data dataframe
+        # for each row in data dataframe
 
-        # check action -- if equals "drop" then break
+            # check action -- if equals "drop" then break
 
-        # else:
+            # else:
 
-            # if
+                # if
 
-    return 0
-    
+        return 0
+
 # TESTING
-tdata = loadData()
-calculateMembership(tdata)
+data = MembershipCalc()
+data.load_data()
+data.calculate_membership()
 # destPath = r'C:/Users/daniel/Desktop/'
 #
 # fullDestPath = os.path.join(destPath, newFile)
